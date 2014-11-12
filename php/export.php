118955087d02805a3543f90a690a426edae21a60
Gabriele Formenti <gabriele.formenti@voome.it>
2014-11-12 23:28:14 +0100

    db export table list

--- php/export.php
+++ php/export.php
@@ -1,1 +1,22 @@
 <?php
+
+$dbHost = 'localhost';
+$dbUser = 'username';
+$dbPass = 'password';
+$dbName = 'test';
+
+$mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+if ($result1 = $mysqli->query("SHOW TABLES FROM $dbName")) {
+    if ($result1->num_rows > 0) {
+        $tables = array();
+        while ($obj = $result1->fetch_object()) {
+            array_push($tables, $obj->{'Tables_in_' . $dbName});
+        }
+    }
+}
